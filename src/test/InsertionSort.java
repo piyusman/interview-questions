5ebf16489fa2106daf7b6991fcfb2a0417c8e8d6
Piyush Mantri <piyushmantri@Piyushs-MacBook-Pro.local>
2017-08-27 15:20:04 -0700

    Logs added

--- src/test/InsertionSort.java
+++ src/test/InsertionSort.java
@@ -3,6 +3,7 @@ package test;
 public class InsertionSort {
  public int[] doInsertionSort(int[] arr){
      int temp;
+     System.out.print("Hello");
      for(int i =1; i<arr.length-1;i++){
          for(int j=i;j>0;j--){
              if(arr[j]<arr[j-1]){
